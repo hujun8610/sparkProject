e7076d878a09303a1d7d4ebd52eac223e4efd1d2
hujun8610 <hujun8610@gmail.com>
2016-01-29 19:38:39 +0800

    解决scala泛型转具体类型的问题 增加GraphUtils的单元测试

--- graphxExercise/src/main/scala/com/bupt/bigdata/example/GraphUtils.scala
+++ graphxExercise/src/main/scala/com/bupt/bigdata/example/GraphUtils.scala
@@ -5,6 +5,7 @@ import org.apache.spark.graphx.Edge
 import org.apache.spark.rdd.RDD
 
 import scala.reflect.ClassTag
+import scala.reflect.runtime.universe._
 
 /**
  * Created by hujun on 2016/1/24.
@@ -12,14 +13,24 @@ import scala.reflect.ClassTag
 object GraphUtils {
 
   def getEdgesFromFile[T:ClassTag](sc:SparkContext,filePath:String):RDD[Edge[T]]={
+    val tClass = scala.reflect.classTag[T]
+    val manifest =  scala.reflect.Manifest
+
     val edgeRdd = sc.textFile(filePath).map(line=>{
       val array = line.split(",")
       val srcId = array(0).toLong
       val desId = array(1).toLong
-      val weight = array(2).asInstanceOf[T]
-      new Edge[T](srcId,desId,weight)
+      val weight = array(2)
+      new Edge(srcId,desId,weight)
     })
-    edgeRdd
+
+    val resultRdd = tClass match {
+      case manifest.Int => edgeRdd.map(e=>new Edge(e.srcId,e.dstId,e.attr.toInt.asInstanceOf[T]))
+      case manifest.Long => edgeRdd.map(e=>new Edge(e.srcId,e.dstId,e.attr.toLong.asInstanceOf[T]))
+      case manifest.Double => edgeRdd.map(e=>new Edge(e.srcId,e.dstId,e.attr.toDouble.asInstanceOf[T]))
+      case _ => throw new Exception("Not support type=" + tClass.toString())
+    }
+    resultRdd
   }
 
 
