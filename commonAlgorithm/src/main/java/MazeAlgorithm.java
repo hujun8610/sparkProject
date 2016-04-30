7d5080ee1ffe7ebc11e4e9d1fd190b6728800a76
hujun8610 <hujun8610@gmail.com>
2016-04-30 16:45:40 +0800

    迷宫 测试数据 .gitignore

---
+++ commonAlgorithm/src/main/java/MazeAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by hujun on 2016/3/24.
+ */
+public class MazeAlgorithm {
+
+    public Stack<Point> searchMazeBFS(int[][] maze){
+        
+
+
+
+
+        return null;
+    }
+
+
+}
