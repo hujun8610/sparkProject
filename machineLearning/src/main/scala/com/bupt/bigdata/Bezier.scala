a7813bc3a537cf8d76338c3101bbd6ba8493b89e
hujun8610 <hujun8610@gmail.com>
2016-06-14 07:06:57 +0800

    new file:   machineLearning/src/main/scala/com/bupt/bigdata/Bezier.scala

---
+++ machineLearning/src/main/scala/com/bupt/bigdata/Bezier.scala
@@ -0,0 +1,21 @@
+package com.bupt.bigdata
+
+/**
+ * Created by hujun on 2016/4/30.
+ */
+class Bezier {
+  import Bezier._
+
+  def cubic_bezier(p1: Point, p2: Point): Double=>Double ={
+
+
+
+    null
+  }
+
+}
+
+object Bezier{
+  case class Point(x: Double, y: Double)
+
+}
