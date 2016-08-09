91cf8048c83a7a648602938a6faf6cdb7b1324b6
hujun <hujun8610@gmail.com>
2016-08-09 08:35:37 +0800

    add read and accpect data

--- commonAlgorithm/src/main/java/com/bupt/javalearning/nio/TCProtocol.java
+++ commonAlgorithm/src/main/java/com/bupt/javalearning/nio/TCProtocol.java
@@ -1,15 +1,37 @@
 package com.bupt.javalearning.nio;
 
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 /**
  * Created by hujun on 2016/8/8.
  */
 public class TCProtocol implements Protocol{
+    private static int ALLOCATE_SIZE = 1024;
+
     @Override
     public void handleRead(SelectionKey key) {
-
+        ByteBuffer bf = ByteBuffer.allocate(ALLOCATE_SIZE);
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        try {
+            while (true){
+                int readBytes = socketChannel.read(bf);
+                if(readBytes>0){
+                    //Todo read data from bf
+                    String data = new String(bf.array(),0,readBytes);
+                    System.out.println(data);
+                    bf.clear();
+                }else{
+                    break;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -19,6 +41,13 @@ public class TCProtocol implements Protocol{
 
     @Override
     public void handleAccepted(SelectionKey key) {
-
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
+        try {
+            SocketChannel socket = serverSocketChannel.accept();
+            socket.configureBlocking(false);
+            socket.register(key.selector(),SelectionKey.OP_READ);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
